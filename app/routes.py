115e8b0011990c0663ee139fbec5f5c91a618600
tri <tri.beleta@gmail.com>
2022-11-03 17:34:20 +0800

    created master layout / removed unnecessary static files

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,7 @@ import app.model as model
 routes = Blueprint('routes', __name__)
 @routes.route('/', methods=['GET', 'POST'])
 def dashboard():
-    return render_template("index.html")
+    return render_template("index.html", title = 'Dashboard')
 
 
 # @login_required
@@ -39,7 +39,7 @@ def dashboard():
 @routes.route('/students', methods=[ 'GET', 'POST'])
 def stud_list():
     students = model.student.all()
-    return render_template('students.html', data = students)
+    return render_template('students.html', data = students, title = 'Students')
 
 @routes.route('/students/add-student', methods=['GET', 'POST'])
 def stud_add():
@@ -52,7 +52,7 @@ def stud_add():
         student = model.student(id= form.id.data, name = form.name.data, course_code = form.course_code.data,  year  =form.year.data, gender = form.gender.data)
         student.add()
         return redirect('/students')
-    return render_template('add-student.html', form = form)
+    return render_template('add-student.html', form = form, title = 'Add Student')
 
 @routes.route('/students/edit-student/<id>', methods=['GET', 'POST'])
 def stud_edit(id):
@@ -68,7 +68,7 @@ def stud_edit(id):
         form.course_code.data = details[0][2]
         form.year.data = details[0][3]
         form.gender.data = details[0][4]
-        return render_template('edit-student.html', form = form)
+        return render_template('edit-student.html', form = form ,title = 'Edit Student')
     elif request.method == 'POST' and form.validate():
         student = model.student( name = form.name.data, course_code = form.course_code.data,  year  =form.year.data, gender = form.gender.data)
         student.edit(id)
@@ -77,7 +77,7 @@ def stud_edit(id):
 @routes.route('/students/student-details/<id>', methods=['GET', 'POST'])
 def stud_open(id):
     details = model.student.open(id)
-    return render_template('student-details.html', data = details)
+    return render_template('student-details.html', data = details, title = 'Student Details')
 
 @routes.route('/students/delete/ <id>', methods=['GET','POST'])
 def stud_delete(id):
@@ -88,7 +88,7 @@ def stud_delete(id):
 @routes.route('/courses', methods=['GET', 'POST'])
 def course_list():
     courses = model.course.all()
-    return render_template('course.html' , data = courses)
+    return render_template('course.html' , data = courses, title = 'Courses')
 
 @routes.route('/courses/add-course', methods=['GET', 'POST'])
 def course_add():
@@ -101,7 +101,7 @@ def course_add():
         course = model.course(course_code= form.course_code.data, course_name = form.course_name.data, college_code = form.college_code.data)
         course.add()
         return redirect('/courses')
-    return render_template('add-course.html', form = form)
+    return render_template('add-course.html', form = form, title = 'Add Course')
 
 @routes.route('/courses/edit-course/<id>', methods=['GET', 'POST'])
 def course_edit(id):
@@ -115,7 +115,7 @@ def course_edit(id):
         form.course_code.data = details[0][0]
         form.course_name.data = details[0][1]
         form.college_code.data = details[0][2]
-        return render_template('edit-course.html', form = form)
+        return render_template('edit-course.html', form = form, title = 'Edit Course')
     elif request.method == 'POST' and form.validate():
         course = model.course( course_name = form.course_name.data, college_code = form.college_code.data)
         course.edit(id)
@@ -124,7 +124,7 @@ def course_edit(id):
 @routes.route('/courses/course-details/<id>', methods=['GET', 'POST'])
 def course_open(id):
     details = model.course.open(id)
-    return render_template('course-details.html', data = details)
+    return render_template('course-details.html', data = details, title = 'Course Details')
 
 @routes.route('/courses/delete/ <id>', methods=['GET','POST'])
 def course_delete(id):
@@ -136,7 +136,7 @@ def course_delete(id):
 @routes.route('/colleges', methods=['GET', 'POST'])
 def college_list():
     colleges = model.college.all()
-    return render_template('college.html' , data = colleges)
+    return render_template('college.html' , data = colleges, title = 'Colleges')
 
 @routes.route('/colleges/add-college', methods=['GET', 'POST'])
 def college_add():
@@ -145,7 +145,7 @@ def college_add():
         college = model.college(college_code= form.college_code.data, college_name= form.college_name.data)
         college.add()
         return redirect('/colleges')
-    return render_template('add-college.html', form = form)
+    return render_template('add-college.html', form = form, title = 'Add College')
 
 @routes.route('/colleges/edit-college/<id>', methods=['GET', 'POST'])
 def college_edit(id):
@@ -154,7 +154,7 @@ def college_edit(id):
     if request.method == 'GET':
         form.college_code.data = details[0][0]
         form.college_name.data = details[0][1]
-        return render_template('edit-college.html', form = form)
+        return render_template('edit-college.html', form = form, title = 'Edit College')
     elif request.method == 'POST' and form.validate():
         college = model.college( college_name = form.college_name.data)
         college.edit(id)
@@ -163,7 +163,7 @@ def college_edit(id):
 @routes.route('/colleges/college-details/<id>', methods=['GET', 'POST'])
 def college_open(id):
     details = model.college.open(id)
-    return render_template('college-details.html', data = details)
+    return render_template('college-details.html', data = details, title = 'College details')
 
 @routes.route('/colleges/delete/ <id>', methods=['GET','POST'])
 def college_delete(id):
